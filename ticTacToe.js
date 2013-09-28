0808226b797717b109d65be4d33fbd4d5ed603ff
Seanna Musgrave <evilseanbot@gmail.com>
2013-09-28 01:23:40 -0700

    Moved CS and JS to own files, cleaned up code, moved message box to side, etc

---
+++ ticTacToe.js
@@ -0,0 +1,171 @@
+angular.module('ticTacToe', []);
+
+function Ctrl($scope, $http) {			
+    $scope.endState = function(message) {
+		$scope.outcomeText = message;
+		$scope.messageVisible= true;
+		$scope.gameOver = true;	    
+	}
+	
+	// AI marks a space and checks endgame conditions.
+	
+	$scope.aiMove = function(playerMove, computerTeam) {
+		$scope.spots[$scope.solution[playerMove].response].state = computerTeam;
+		$scope.solution = $scope.solution[playerMove];
+		if ($scope.won($scope.spots)) {
+			$scope.endState("Computer won");
+		} else if ($scope.draw($scope.spots)) {
+			$scope.endState("Draw");
+		}	
+	}
+
+	// If game is still continuing, marks the space for player and checks endgame conditions.
+	
+	$scope.fillIn = function(spot) {
+	    if ($scope.team == 'X') {
+		    var computerTeam = "O";
+		} else {
+		    var computerTeam = "X";
+	    }
+		
+	    if (!$scope.gameOver) {
+			if (spot.state == "") {
+				spot.state = $scope.team;
+										
+				if ($scope.won($scope.spots)) {
+				    // Unused code :)
+				    $scope.endState("You won");
+				} else if ($scope.draw($scope.spots)) {
+				    $scope.endState("Draw");
+                } else {						
+				    $scope.aiMove(spot.id, computerTeam);
+				}
+			}
+		}
+	}
+	
+	// Checks to see if the game is a draw.
+	
+	$scope.draw = function(spots) {
+	    var filledSpots = 0;
+	    for (i = 0; i < spots.length; i++) {
+		    if (spots[i]["state"] != '') {
+			    filledSpots++;
+			}
+		}
+		
+		if (filledSpots == 9) {
+		    return true;
+		} else {
+		    return false;
+		}
+	}
+	
+	// Checks to see if the game has been won by either party.
+	
+	$scope.getLines = function() {
+	    return [[0, 1, 2],
+		        [3, 4, 5],
+				[6, 7, 8],
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+				[0, 4, 8],
+				[2, 4, 6]];
+	}
+	
+	$scope.won = function(spots) {
+	    var teams = ["O", "X"];
+		var lines = $scope.getLines();
+	    for (var team = 0; team < 2; team++) {
+		    for (var line = 0; line < lines.length; line++) {
+			    var filledSpots = 0;
+				for (var pos = 0; pos < lines[line].length; pos++) {
+					if (spots[lines[line][pos]].state == teams[team]) {
+						filledSpots++;
+					}
+				}
+				if (filledSpots == 3) {
+				    return true;
+				}				
+			}
+		}
+		return false;		
+	};	
+	
+	$scope.getSolution = function() {
+	    if ($scope.team == 'X') {
+		    solutionFile = 'TTTOSolution.json';
+		} else {
+		    solutionFile = 'TTTXSolution.json';
+		}
+	
+	    $http.get(solutionFile).success( function(solutionData) {
+		    $scope.solution = solutionData;
+			if ($scope.team == "O") {
+				$scope.spots[$scope.solution["response"]].state = "X";
+			}
+			$scope.messageVisible = false;			
+			$scope.gameOver = false;
+		});
+	
+	}
+	
+	// Creates the tic tac toe grid.
+	
+	$scope.getSpots = function() {
+        var spots = [];		
+		for (var y = 0; y < 3; y++) {
+			for (var x = 0; x < 3; x++) {
+				spots.push({row: x, col: y, state: "", id: x+(y*3), classes: []});
+				
+				var cssClasses = [];
+				
+				if (y == 0) {
+				    cssClasses.push("topSpot");
+				} else if (y == 1) {
+				    cssClasses.push("midYSpot");
+				} else {
+				    cssClasses.push("bottomSpot");
+				}
+
+				if (x == 0) {
+				    cssClasses.push("leftSpot");
+				} else if (x == 1) {
+				    cssClasses.push("midXSpot");
+				} else {
+				    cssClasses.push("rightSpot");
+				}
+				
+				spots[spots.length-1].classes = cssClasses;
+				
+			}
+	    }
+		return spots;
+	
+	}
+	
+	$scope.startGame = function() {
+		$scope.endState("Loading...");
+		
+		$scope.getSolution();
+		$scope.spots = $scope.getSpots();		
+	}
+	
+	$scope.team = "X";
+	$scope.startGame();
+}
+
+// Function taken from Stack Overflow user ConroyP
+// http://stackoverflow.com/questions/122102/most-efficient-way-to-clone-an-object
+
+function clone(obj){
+    if(obj == null || typeof(obj) != 'object')
+        return obj;
+
+    var temp = obj.constructor(); // changed
+
+    for(var key in obj)
+        temp[key] = clone(obj[key]);
+    return temp;
+}
--- ticTacToe.py
+++ ticTacToe.py
@@ -5,6 +5,10 @@ import json
 theoryCount = 0
 before = time.time()
 
+# This generates an ideal move to make given the current spots.
+# forEnemy is a boolean determines whether the AI is making a move for itself, or its enemy (theoretical human player playing with the same intelligence)
+# makeBest Move returns an object containing the best outcome if it plays perfectly(1 for win, 0 for draw, -1 for lose) and the position (pos) (index on the spot array) of its next move.
+
 def makeBestMove(spots, lines, forEnemy):
     global theoryCount
     bestMove = {}
@@ -13,12 +17,14 @@ def makeBestMove(spots, lines, forEnemy):
 
     for i in range(9):
         if spots[i] == "":
-            
-            if forEnemy:
-                spots[i] = "X"
-            else:
-                spots[i] = "O"
 
+            # The enemy's (Or theoretical ideal human opponent) marks are listed as "E", and the AIs marks are listed as "A"
+            if forEnemy:
+                spots[i] = "E"
+            else:
+                spots[i] = "A"
+                
+            # This keeps a running total of how many theoretical moves the AI looks into, used for testing purposes.
             theoryCount += 1
 
             if getGameOverState(spots, lines)["gameOver"]:
@@ -33,8 +39,12 @@ def makeBestMove(spots, lines, forEnemy):
                 bestMove["pos"] = i
     return bestMove
 
+# Returns the results from a line being filled out.
+# -1 is a loss.
+# 1 is a win.
+
 def getResultFromCross(team):
-    if team == "X":
+    if team == "E":
         return {"gameOver": True, "outcome": -1}
     else:
         return {"gameOver": True, "outcome": 1}    
@@ -57,7 +67,7 @@ def getLines():
 
 def getGameOverState(spots, lines):
     # First checks to see if someone has won:
-    for team in ["X", "O"]:
+    for team in ["E", "A"]:
         for line in getLines():
             filledSpots = 0
             for pos in line:
@@ -76,46 +86,45 @@ def getGameOverState(spots, lines):
     else:
         return {"gameOver": False, "outcome": None}
 
+# Prints out the current results in a grid (Used for testing purposes)
 
 def printToes(spots):
     for y in range(3):
         for x in range(3):
-            if spots[x + (y*3)] == "X":
-                print "X",
-            elif spots[x + (y*3)] == "O":
-                print "O",
+            if spots[x + (y*3)] == "E":
+                print "E",
+            elif spots[x + (y*3)] == "A":
+                print "A",
             else:
                 print "_",
         print ""
     print "="
 
+# Creates a "Solution Branch", An ideal  response as well as a list of possible opponent moves that could follow the response, which will have 
+
 def getSolutionBranch(spots, lines):
     branch = {}
     branch["response"] = makeBestMove(spots, lines, False)["pos"]
-    spots[branch["response"]] = "O"
+    spots[branch["response"]] = "A"
     if not getGameOverState(spots, lines)["gameOver"]:
         for i in range(9):
             if spots[i] == "":
-                spots[i] = "X"
+                spots[i] = "E"
                 if not getGameOverState(spots, lines)["gameOver"]:
                     branch[i] = getSolutionBranch(spots, lines)
                 spots[i] = ""
     spots[branch["response"]] = ""
     return branch
-            
+
+# Creates a starting solution base for players who are starting second, which does not have a starting "Response" assumed in getSolution Branch.
 
 def getSolutionForO(spots, lines):
     solution = {}
     for i in range(9):
-        spots[i] = "X"
+        spots[i] = "E"
         solution[i] = {}
         solution[i] = getSolutionBranch(spots, lines)
         spots[i] = ""
-    return solution
-
-def getSolutionForX(spots, lines):
-    solution = {}
-    solution = getSolutionBranch(spots, lines)
     return solution
 
 def prettyJson(string):
@@ -133,12 +142,8 @@ fo = open("TTTOSolution.json", "wb")
 fo.write(OSolution);
 fo.close()
 
-XSolution = prettyJson(getSolutionForX(spots, lines))
+XSolution = prettyJson(getSolutionBranch(spots, lines))
 
 fo = open("TTTXSolution.json", "wb")
 fo.write(XSolution);
 fo.close()
-
-# print(MyBestMove(spots))
-# print "Theory Count for solution: " + str(theoryCount)
-# print "Time for solution: " + str(time.time() - before)
